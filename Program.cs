587c2eeff1c512cd4203724bd534fae413ffe285
robersonruas <robersonruas@hotmail.com>
2024-04-17 21:17:56 -0300

    Desafio Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("123-123","1234","123456",8);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone("123-123","1234","123456",8);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
 // TODO: Realizar os testes com as classes Nokia e Iphone
